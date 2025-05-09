b59fa8876a4bd284e0c8dea9906ea6fd49814093
chistym17 <mchisty31@gmail.com>
2025-05-10 00:28:49 +0600

    footer and newsletter section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import WorkflowSection from "./components/WorkflowSection";
 import JobListings from "./components/JobListings";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +16,11 @@ export default function Home() {
       <div id="joblistings">
         <JobListings />
       </div>
+      <div>
+        <Newsletter/>
+      </div>
+      <Footer/>
+
     </div>
   );
 }
